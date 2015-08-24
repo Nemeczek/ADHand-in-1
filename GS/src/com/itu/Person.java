8a2d985d204d5ccc7ed9fef58a697139022297a2
Nemeczek <nemeczekes@hotmail.com>
2015-08-24 11:38:15 +0200

    hard codded input

--- GS/src/com/itu/Person.java
+++ GS/src/com/itu/Person.java
@@ -4,4 +4,16 @@ public class Person {
     String name;
     int[] preferences;
     boolean isMatched;
+    boolean isMale;
+
+    public Person(String name, int[] preferences, boolean isMale) {
+        this.name = name;
+        this.preferences = preferences;
+        this.isMale = isMale;
+    }
+
+    public Person(String name, int[] preferences) {
+        this.name = name;
+        this.preferences = preferences;
+    }
 }
