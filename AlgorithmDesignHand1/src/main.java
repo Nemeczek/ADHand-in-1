9b3dfb113af513c02abf7432ac54474361d24df7
Stelios Papaoikonomou <spapaoikonomou@hotmail.com>
2015-08-31 15:35:21 +0200

    thefinaloneornot?

--- AlgorithmDesignHand1/src/main.java
+++ AlgorithmDesignHand1/src/main.java
@@ -1,14 +1,9 @@
-/**
- * Created by Stelios on 24/8/2015.
- */
 import java.util.*;
 import java.io.*;
 import java.lang.System;
 
 public class main {
-
-       private static String[] ReadFile(String path) throws IOException
-       {
+       private static String[] ReadFile(String path) throws IOException {
               File file = new File(path);
               FileInputStream fis = new FileInputStream(file);
               byte[] data = new byte[(int) file.length()];
@@ -18,8 +13,7 @@ public class main {
               return fileString.split("\n");
        }
 
-       public static void main(String [] args) throws IOException
-       {
+       public static void main(String [] args) throws IOException {
            String[] fileLines = ReadFile(args[0]);
            int n = 0;
            for (int i=0 ; i < fileLines.length ; i++){
@@ -28,59 +22,42 @@ public class main {
                        break;
                }
            }
-           if (n==0){
-               System.out.println("Number n missing from config file");
-           }
+           if (n==0) System.out.println("Number n missing from config file");
+
            //reading names for fun
-           String [] mNames = new String[n];
-           String [] wNames = new String[n];
+           String [] mNames = new String[n], wNames = new String[n];
            int j = 1;
            for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
                if (fileLines[i].startsWith(Integer.toString(j) + " ")) {
                    if (j%2==0) {
-                       wNames[j / 2 - 1] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1))); //size of n+ space
-                       //System.out.println( j + " is " + wNames[j/2-1] + " girl");
+                       wNames[j / 2 - 1] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1)));
                    }
                    else {
-                       mNames[(j - 1) / 2] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1))); //size of n+ space
-                       //System.out.println( j + " is " + mNames[(j - 1) / 2] + " guy");
+                       mNames[(j - 1) / 2] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1)));
                    }
                    j++;
                }
            }
-           Integer[][] wprefs = new Integer[n][n];
-           Integer[][] mprefs = new Integer[n][n];
+           Integer[][] wprefs = new Integer[n][n], mprefs = new Integer[n][n];
            j = 1;
            for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
                if (fileLines[i].startsWith(Integer.toString(j) + ": ")) {
                    String [] tempStr = fileLines[i].substring(fileLines[i].lastIndexOf(":")+2).split(" ");
-                   //System.out.printf(wNames[(j - 1)/2] + " woman");
                    for (int k=0 ; k<n ; k++) {
-                       mprefs[(j - 1)/2][k]=Integer.parseInt(tempStr[k])/2-1; //size of n+ space
-                       //System.out.printf(" " + wprefs[(j - 1)/2][k]);
+                       mprefs[(j - 1)/2][k]=Integer.parseInt(tempStr[k])/2-1;
                    }
-                   //System.out.println();
                    j=j+2;
                }
            }
-
 
            j = 2;
            for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
                if (fileLines[i].startsWith(Integer.toString(j) + ": ")) {
                    String [] tempStr = fileLines[i].substring(fileLines[i].lastIndexOf(":")+2).split(" ");
                    for (int k=0 ; k<n ; k++) {
-                       wprefs[j/2-1][k]=(Integer.parseInt(tempStr[k])-1)/2; //size of n+ space
+                       wprefs[j/2-1][k]=(Integer.parseInt(tempStr[k])-1)/2;
                    }
                    j=j+2;
-               }
-           }
-
-           //creating inverted wprefs
-           Integer[][] invertedWprefs = new Integer[n][n];
-           for (int i = 0 ; i < n ; i++) {
-               for (int k = 0; k < n; k++) {
-                   invertedWprefs[i][wprefs[i][k]]=k;
                }
            }
 
@@ -108,7 +85,7 @@ public class main {
            for (int i=0;i<n;i++){
                wMatches[i]=-1;
            }
-        int x=-1,y=-1;
+        int womansNewguysPr=-1,womansCurrBfPr=-1;
            while (!unmatched.empty()) {
                int guy = unmatched.peek();
                int nextGirlInMPrefs = propose[guy];
@@ -116,7 +93,6 @@ public class main {
                    System.out.printf(mNames[guy] + " proposes to ");
                    int womanHeWantsNow = mprefs[guy][nextGirlInMPrefs];
                    System.out.printf(wNames[womanHeWantsNow]);
-
                    if (wMatches[womanHeWantsNow]==-1) {
                        System.out.printf(" and she is single.\n");
                        mMatches[guy] = womanHeWantsNow;
@@ -125,17 +101,16 @@ public class main {
                        propose[guy]++;
                        break;
                    }
-                   //}else if(invertedWprefs[guy][womanHeWantsNow] < invertedWprefs[wMatches[womanHeWantsNow]][womanHeWantsNow]) {
-                       for (int i=0;i<n;i++){ //find the woman's preference of this guy
-                           if (wprefs[womanHeWantsNow][i]==guy)
-                               x = i;
-                       }
-                       for (int i=0;i<n;i++){ //find the woman's preference of this guy
-                           if (wprefs[womanHeWantsNow][i]==wMatches[womanHeWantsNow])
-                               y = i;
-                       }
-                   System.out.printf(" " + x + " " +y);
-                       if(x < y) {
+                   for (int i=0;i<n;i++){ //find the woman's preference of this guy
+                   if (wprefs[womanHeWantsNow][i]==guy)
+                       womansNewguysPr = i;
+                   }
+                   for (int i=0;i<n;i++){ //find the woman's preference of this guy
+                       if (wprefs[womanHeWantsNow][i]==wMatches[womanHeWantsNow])
+                           womansCurrBfPr = i;
+                   }
+                   System.out.printf(" " + womansNewguysPr + " " +womansCurrBfPr);
+                   if(womansNewguysPr < womansCurrBfPr) {
                        System.out.println(" and although she is already with " + mNames[wMatches[womanHeWantsNow]]);
                        System.out.println(" she prefers " + mNames[guy]);
                        unmatched.pop();
@@ -143,23 +118,17 @@ public class main {
                        mMatches[wMatches[womanHeWantsNow]]=-1;
                        wMatches[womanHeWantsNow] = guy;
                        mMatches[guy] = womanHeWantsNow;
-
                        propose[guy]++;
                        break;
                    }else{
                        System.out.println(" but she is with " + mNames[wMatches[womanHeWantsNow]] + " and she prefers him.");
                        propose[guy]++;
                        nextGirlInMPrefs = propose[guy];
-                       //continuing the hunt
                    }
-
-
                }
-
            }
            for (int i=0;i<n;i++){
-               //System.out.println( i + " -- " + mMatches[i]);
                System.out.println(mNames[i] + " -- " + wNames[mMatches[i]]);
            }
-       };
+       }
 }
