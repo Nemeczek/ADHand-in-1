4aa0937e841db9dfe5e51ac59ec9a7e1e19e5a14
RLOC <rloc@itu.dk>
2015-09-02 11:03:19 +0200

    Added code to "prod" project

---
+++ Hand-In-1-Stable-Matching/src/com/company/GS.java
@@ -0,0 +1,135 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Stack;
+
+/**
+ * Created by GROUP C on 02-09-2015.
+ */
+class GaleShapley {
+
+    public static void StableMatcher (String[] file) throws IOException {
+        String[] fileLines = file;
+        int n = 0;
+        for (int i=0 ; i < fileLines.length ; i++){
+            if (fileLines[i].contains("n=")) {
+                n = Integer.parseInt(fileLines[i].substring(2));
+                break;
+            }
+        }
+        if (n==0) System.out.println("Number n missing from config file");
+
+        //reading names for fun
+        String [] mNames = new String[n], wNames = new String[n];
+        int j = 1;
+        for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
+            if (fileLines[i].startsWith(Integer.toString(j) + " ")) {
+                if (j%2==0) {
+                    wNames[j / 2 - 1] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1)));
+                }
+                else {
+                    mNames[(j - 1) / 2] = new String(fileLines[i].substring((int) (Math.log10(j) + 1 + 1)));
+                }
+                j++;
+            }
+        }
+        Integer[][] wprefs = new Integer[n][n], mprefs = new Integer[n][n];
+        j = 1;
+        for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
+            if (fileLines[i].startsWith(Integer.toString(j) + ": ")) {
+                String [] tempStr = fileLines[i].substring(fileLines[i].lastIndexOf(":")+2).split(" ");
+                for (int k=0 ; k<n ; k++) {
+                    mprefs[(j - 1)/2][k]=Integer.parseInt(tempStr[k])/2-1;
+                }
+                j=j+2;
+            }
+        }
+
+        j = 2;
+        for (int i=0 ; i<=fileLines.length && j<=2*n ; i++) {
+            if (fileLines[i].startsWith(Integer.toString(j) + ": ")) {
+                String [] tempStr = fileLines[i].substring(fileLines[i].lastIndexOf(":")+2).split(" ");
+                for (int k=0 ; k<n ; k++) {
+                    wprefs[j/2-1][k]=(Integer.parseInt(tempStr[k])-1)/2;
+                }
+                j=j+2;
+            }
+        }
+
+        //create unmatched man list
+        Stack<Integer> unmatched = new Stack();
+        for (int i=n-1;i>=0;i--){
+            unmatched.push(i);
+        }
+        //create table of proposed girls
+        Integer[] propose = new Integer[n];
+        //fill it with zeroes
+        //it means the number of the girl in the guy's prefs
+        for (int i=0;i<n;i++){
+            propose[i]=0;
+        }
+        //create table of men's matches
+        Integer[] mMatches = new Integer[n];
+        //fill it with -1
+        for (int i=0;i<n;i++){
+            mMatches[i]=-1;
+        }
+        //create table of women's matches
+        Integer[] wMatches = new Integer[n];
+        //fill it with -1
+        for (int i=0;i<n;i++){
+            wMatches[i]=-1;
+        }
+        int womansNewguysPr=-1,womansCurrBfPr=-1;
+        while (!unmatched.empty()) {
+            int guy = unmatched.peek();
+            int nextGirlInMPrefs = propose[guy];
+            while (nextGirlInMPrefs<n){
+                // debug sout
+                // System.out.printf(mNames[guy] + " proposes to ");
+                int womanHeWantsNow = mprefs[guy][nextGirlInMPrefs];
+                // debug sout
+                // System.out.printf(wNames[womanHeWantsNow]);
+                if (wMatches[womanHeWantsNow]==-1) {
+                    // debug sout
+                    // System.out.printf(" and she is single.\n");
+                    mMatches[guy] = womanHeWantsNow;
+                    wMatches[womanHeWantsNow] = guy;
+                    unmatched.pop();
+                    propose[guy]++;
+                    break;
+                }
+                for (int i=0;i<n;i++){ //find the woman's preference of this guy
+                    if (wprefs[womanHeWantsNow][i]==guy)
+                        womansNewguysPr = i;
+                }
+                for (int i=0;i<n;i++){ //find the woman's preference of this guy
+                    if (wprefs[womanHeWantsNow][i]==wMatches[womanHeWantsNow])
+                        womansCurrBfPr = i;
+                }
+                // debug sout
+                // System.out.printf(" " + womansNewguysPr + " " +womansCurrBfPr);
+                if(womansNewguysPr < womansCurrBfPr) {
+                    // debug sout
+                    // System.out.println(" and although she is already with " + mNames[wMatches[womanHeWantsNow]]);
+                    // System.out.println(" she prefers " + mNames[guy]);
+                    unmatched.pop();
+                    unmatched.push(wMatches[womanHeWantsNow]);
+                    mMatches[wMatches[womanHeWantsNow]]=-1;
+                    wMatches[womanHeWantsNow] = guy;
+                    mMatches[guy] = womanHeWantsNow;
+                    propose[guy]++;
+                    break;
+                }else{
+                    // debug sout
+                    // System.out.println(" but she is with " + mNames[wMatches[womanHeWantsNow]] + " and she prefers him.");
+                    propose[guy]++;
+                    nextGirlInMPrefs = propose[guy];
+                }
+            }
+        }
+        for (int i=0;i<n;i++){
+            System.out.println(mNames[i] + " -- " + wNames[mMatches[i]]);
+        }
+    }
+}
--- Hand-In-1-Stable-Matching/src/com/company/Main.java
+++ Hand-In-1-Stable-Matching/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.IOException;
+
+/**
+ * Created by GROUP C on 02-09-2015.
+ */
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        if (args.length > 0) {
+
+            // Parsing of fileinput - args[0]
+            String[] input = FileParser.ReadFile(args[0]);
+            // Calling Gale Shapley's Stable Matching algorithm
+            GaleShapley.StableMatcher(input);
+
+            System.out.println("\n");
+            System.out.println("Stable Matching complete!");
+        }
+        else {
+            System.out.println("The program needs a filepath as input \n");
+            System.out.println("Terminating ...");
+        }
     }
 }
