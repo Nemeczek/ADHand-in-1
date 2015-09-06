308b6a1968ed74bf3d13e1dec6ceae227b9527eb
RLOC <rloc@itu.dk>
2015-09-06 15:31:39 +0200

    initial parser

--- HandIn2/src/com/groupC/Parser.java
+++ HandIn2/src/com/groupC/Parser.java
@@ -1,4 +1,20 @@
 package com.groupC;
 
 public class Parser {
+
+    public static Job[] Parse (String[] file) {
+
+        // the input file split into lines
+        String[] fileLines = file;
+
+        // the initial Job array is created
+        Job[] jobs = new Job[];
+
+        // creating the array of Job's
+        // i=2 due to the fact that input file always contains "n line" + "empty line"
+        for (int i=2; i < fileLines.length; i++) {
+
+        }
+
+    }
 }
