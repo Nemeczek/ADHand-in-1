0266826fe6d459e3a740e06aa6c6a97f6862a2aa
Stelios Papaoikonomou <spapaoikonomou@hotmail.com>
2015-09-06 15:29:30 +0200

    IP
    fdsd

--- HandIn2/src/com/groupC/IP.java
+++ HandIn2/src/com/groupC/IP.java
@@ -1,4 +1,24 @@
 package com.groupC;
 
+import java.io.IOException;
+
 public class IP {
+ public static void main(String[] args) throws IOException {
+     if (args.length > 0) {
+
+         // Parsing of fileinput - args[0]
+         String[] input = FileReader.ReadFile(args[0]);
+        // GaleShapley gs = Transformer.Transformation(input);
+
+         //gs.StableMatcher();
+
+         System.out.println("\n");
+         System.out.println("Interval partitioning complete!");
+     }
+     else {
+         System.out.println("The program needs a filepath as input \n");
+         System.out.println("Terminating ...");
+     }
+
+ }
 }
