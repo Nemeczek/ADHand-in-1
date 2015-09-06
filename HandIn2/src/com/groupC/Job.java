a695b84ec9e5350620bd84e252787f868cd1eca1
Stelios Papaoikonomou <spapaoikonomou@hotmail.com>
2015-09-06 15:19:57 +0200

    Job class

--- HandIn2/src/com/groupC/Job.java
+++ HandIn2/src/com/groupC/Job.java
@@ -1,4 +1,23 @@
 package com.groupC;
 
 public class Job {
+    int start = 0;
+    int end = 0;
+    int resource = -1;
+    public Job(int s, int e){
+        start = s;
+        end = e;
+    }
+    public void setResource(int r){
+        resource = r;
+    }
+    public int getStart(){
+        return start;
+    }
+    public int getEnd(){
+        return end;
+    }
+    public int getResource(){
+        return resource;
+    }
 }
